cfc31e8be8c48c86510e7b2a3bef3d88d9fd48b9
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-05-22 15:56:49 +0500

    noprofileAvtar changed and direcotry updated

--- packages/pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
@@ -6,6 +6,7 @@ const Icon: React.FC<SvgProps> = (props) => {
  
   return (
     <Svg viewBox="0 0 32 32" {...props}>
+      
            <image width="30" height="30"  href='https://swap.nowbanq.com/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png'/>
 
     </Svg>
