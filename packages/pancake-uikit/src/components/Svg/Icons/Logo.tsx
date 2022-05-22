83e844e8a2ea614e220a0ab77ed539e60ed92209
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-05-22 15:55:05 +0500

    nodes updated

--- packages/pancake-uikit/src/components/Svg/Icons/Logo.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/Logo.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 198 199" {...props}>
-            <image width="130" height="130" y="30" href='/images/pixel/LogoDark.png'/>
+            <image width="130" height="130" y="30" href='/images/nowbanq/LogoDark.png'/>
 
     </Svg>
   );
