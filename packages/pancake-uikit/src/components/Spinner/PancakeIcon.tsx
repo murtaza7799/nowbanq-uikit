3e1d84d81566c7e413462db02bb4758b25f3d871
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-05-26 03:03:21 +0500

    swap confirmation icon updated

--- packages/pancake-uikit/src/components/Spinner/PancakeIcon.tsx
+++ packages/pancake-uikit/src/components/Spinner/PancakeIcon.tsx
@@ -5,7 +5,7 @@ import { SvgProps } from "../Svg/types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <Svg viewBox="0 0 128 128" {...props}>
-     <image width="50" height="50" href='https://swap.nowbanq.com/images/nowbanq/LogoDark.png'/>
+     <image width="50" height="50" href='https://swap.nowbanq.com/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.png'/>
     </Svg>
   );
 };
