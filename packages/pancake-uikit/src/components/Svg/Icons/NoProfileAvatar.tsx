90132221bb533cf1a6c1514baadd690eb19804e1
Muhammad Murtaza <48488384+murtaza7799@users.noreply.github.com>
2022-06-02 09:01:23 +0500

    fixed

--- packages/pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
+++ packages/pancake-uikit/src/components/Svg/Icons/NoProfileAvatar.tsx
@@ -8,7 +8,7 @@ const Icon: React.FC<SvgProps> = (props) => {
     <Svg viewBox="0 0 32 32" {...props}>
       
            <image width="30" height="30" x="1" href='https://swap.nowbanq.com/images/nowbanq/LogoDark.png'/>
-
+           
     </Svg>
   );
 };
